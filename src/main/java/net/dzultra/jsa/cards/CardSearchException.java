bf79767fc40e575267355665e6924e5fcea9ea66
DZultra <DanielZ123@web.de>
2026-03-28 13:01:46 +0100

    CardSearchException Formatting

--- src/main/java/net/dzultra/jsa/cards/CardSearchException.java
+++ src/main/java/net/dzultra/jsa/cards/CardSearchException.java
@@ -6,6 +6,6 @@ import java.net.URI;
 
 public class CardSearchException extends RuntimeException {
     public CardSearchException(URI uri, @Nullable String response) {
-        super("Issue while sending CardSearch request to " + uri + ". Response: " + response);
+        super("Issue while sending CardSearch request to " + uri + ". Response: \n" + response);
     }
 }
