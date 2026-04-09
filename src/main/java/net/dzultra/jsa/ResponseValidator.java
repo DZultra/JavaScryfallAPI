a94af3193c4ae046ab2ba561365708d9ec2bdb0a
DZultra <DanielZ123@web.de>
2026-04-09 21:11:38 +0200

    Added JsonSyntaxError Handling for the ResponseValidator If Scryfall is on maintenance even the api will reply with Html

--- src/main/java/net/dzultra/jsa/ResponseValidator.java
+++ src/main/java/net/dzultra/jsa/ResponseValidator.java
@@ -1,21 +1,40 @@
 package net.dzultra.jsa;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
 public class ResponseValidator {
     public static boolean isValidResponseSingle(Gson gson, String response, String type) {
-        TypeRecord record = gson.fromJson(response, TypeRecord.class);
-        return record.type().equals(type);
+        try {
+            TypeRecord record = gson.fromJson(response, TypeRecord.class);
+            return record.type().equals(type);
+        } catch (JsonSyntaxException e) {
+            return false;
+        }
     }
 
     public static boolean isValidResponseDouble(Gson gson, String response, String type1, String type2) {
-        DataTypeRecord record = gson.fromJson(response, DataTypeRecord.class);
+        DataTypeRecord record;
+        try {
+            record = gson.fromJson(response, DataTypeRecord.class);
+        } catch (JsonSyntaxException e) {
+            return false;
+        }
         return record.type().equals(type1) && record.data()[0].type().equals(type2);
     }
 
     public static boolean isValidSymbologyResponse(Gson gson, String response) {
-        return (gson.fromJson(response, TypeRecord.class).type().equals("list")
-                && gson.fromJson(response, DataTypeRecord.class).data()[0].type().equals("card_symbol"))
-                || gson.fromJson(response, TypeRecord.class).type().equals("mana_cost");
+        try {
+            TypeRecord typeRecord = gson.fromJson(response, TypeRecord.class);
+            if (typeRecord.type().equals("mana_cost")) {
+                return true;
+            } else if (typeRecord.type().equals("list")) {
+                DataTypeRecord dataRecord = gson.fromJson(response, DataTypeRecord.class);
+                return dataRecord.data()[0].type().equals("card_symbol");
+            }
+            return false;
+        } catch (JsonSyntaxException e) {
+            return false;
+        }
     }
 }
